9c95ae4f6baa09a7f8768fe65adab025c318cf6f
Joshua Melville <joshua.melville@northwestern.edu>
2022-09-15 11:46:42 +0200

    remove exports from package.json because electron isn't importing it correctly

--- tsup.config.js
+++ tsup.config.js
@@ -1,9 +1,9 @@
-import {defineConfig} from 'tsup';
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
   sourcemap: true,
   clean: true,
-  entryPoints: ['src/index.ts', 'src/index.test.ts'],
-  format: ['cjs', 'esm'],
+  entryPoints: ['src/index.ts'],
+  format: ['esm'],
   dts: true,
 });
