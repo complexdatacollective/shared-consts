0e7dec69883228787ebaa926b7e06c6e64672f91
Joshua Melville <joshua.melville@northwestern.edu>
2022-09-15 11:54:32 +0200

    reinstate cjs and minify

--- tsup.config.js
+++ tsup.config.js
@@ -4,6 +4,9 @@ export default defineConfig({
   sourcemap: true,
   clean: true,
   entryPoints: ['src/index.ts'],
-  format: ['esm'],
+  format: ['cjs', 'esm'],
   dts: true,
+  treeshake: true,
+  splitting: true,
+  minify: true,
 });
